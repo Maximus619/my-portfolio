7819b1a15ca8c7e5b631246b8f1fa5be3f9ea7e7
Maximus619 <balttiboy@gmail.com>
2022-08-25 21:46:30 +0530

    Contact page completed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,13 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import Loader from 'react-loaders'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
+
+    const form = useRef;
     const [letterClass, setLetterClass] = useState('text-animate')
 
     useEffect(() => {
@@ -13,6 +17,22 @@ const Contact = () => {
             }, 3000);
         }
     }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm('service_3xnpgvj', 'anant', e.target, 'z_iW-fNysS9S9Pz0T')
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again')
+                }
+            )
+    }
 
     return (
         <>
@@ -25,6 +45,45 @@ const Contact = () => {
                             idx={15}
                         />
                     </h1>
+                    <p>
+                        I am interested in freelance opportunities - espacially ambitious or large projects. However, if you have either request or question, don't hesitate to contact me using below form.
+                    </p>
+                    <div className="contact-form">
+                        <form onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input placeholder="Name" type="text" name="name" required />
+                                </li>
+                                <li className="half">
+                                    <input
+                                        placeholder="Email"
+                                        type="email"
+                                        name="email"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        placeholder="Subject"
+                                        type="text"
+                                        name="subject"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <textarea
+                                        placeholder="Message"
+                                        name="message"
+                                        required
+                                    ></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+
                 </div>
             </div>
             <Loader type="pacman" />
